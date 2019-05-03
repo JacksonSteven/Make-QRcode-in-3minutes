3afe5f7ed94d775dafd23afbff53cd3d96ff025b
JacksonSteven <1013413941@qq.com>
2019-05-03 14:36:09 +0800

    second
    没有颜色二维码的代码和输入输出图片

---
+++ nocolorQR.py
@@ -0,0 +1,10 @@
+# 2.生成黑白底图二维码
+
+from MyQR import myqr
+myqr.run(words='https://github.com/JacksonSteven',
+         picture='image.jpg',
+         
+         save_name='colorQR2.png')
+#  wods输入网址
+# picture输入图片，新手输入图片绝对地址，我的代码中为相对地址
+# save_name 是输出图片的保存名
